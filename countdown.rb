034d66fa6dd8fc82109f88cdeb6645d23f031699
Chris Beyer <beyercorpuz@gmail.com>
2020-01-25 22:20:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
 def countdown
+  x = 1
+while x < 10
+  puts "#{x} Second(S)!is less than 10"
+  x += 1
+  Puts "Happy New Year!"
+  
+end
+  
+
+  
+end
+def countdown_with_sleep(num)
+  sleep(num_secs)
 end
